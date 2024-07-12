f3d7900b4fd19981e9e2e94fc09967738acee423
bismilay <iron-242004@mail.ru>
2024-07-12 22:55:00 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+homeworks='Количество выполненных заданий'
+The_number_of_complete_homeworks=12
+hours='Количество потраченных часов'
+The_number_of_hours_spent=1.5
+time='среднее время выполнения'
+Average_execution_time=0.125
+print('Количество выполненных заданий', The_number_of_complete_homeworks , hours , The_number_of_hours_spent , time , Average_execution_time )
+# В принте попробовал оба варианта записи расшифровки переменной и название самой переменной , получилось^)
